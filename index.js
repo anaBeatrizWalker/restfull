26859e63ead3f21b049544db16cdea542696c2ac
Ana Beatriz <anabiawalker@gmail.com>
2022-01-24 19:11:59 -0300

    Criando o servidor com Express

--- index.js
+++ index.js
@@ -1,31 +1,25 @@
-const http = require('http')
+const express = require('express')
 
-let server = http.createServer((req, res)=>{
-    //linha de comando
-    console.log('URL:', req.url)
-    console.log('METHOD:', req.method)
+let app = express()
 
-    switch(req.url){
-        case '/':
-            res.statusCode = 200 //código OK
-            res.setHeader('Content-Type', 'text/html') //cabeçalho
-            res.end('<h1>Olá</h1>')
-        break;
-
-        case '/users':
-            res.statusCode = 200 //código OK
-            res.setHeader('Content-Type', 'application/json') //cabeçalho
-            res.end(JSON.stringify({
-                users: [{
-                    name: 'Ana',
-                    email: 'aaaa@gmail.com',
-                    id: 1
-                }]
-            }))
-        break;
-    }
+app.get('/', (req, res)=>{
+    res.statusCode = 200 //código OK
+    res.setHeader('Content-Type', 'text/html') //cabeçalho
+    res.end('<h1>Olá</h1>')
 })
 
-server.listen(3000, '127.0.0.1', ()=>{ //porta 3000, IP local
+app.get('/users', (req, res)=>{
+    res.statusCode = 200 //código OK
+    res.setHeader('Content-Type', 'application/json') //cabeçalho
+    res.json({
+        users: [{
+            name: 'Ana',
+            email: 'aaaa@gmail.com',
+            id: 1
+        }]
+    })
+})
+
+app.listen(3000, '127.0.0.1', ()=>{ //porta 3000, IP local
     console.log("Servidor rodando")
 }) 
