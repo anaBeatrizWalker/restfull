67054fba2ed6a425a9b1e6a70a0f45664db0c827
Ana Beatriz <anabiawalker@gmail.com>
2022-01-24 19:01:47 -0300

    Detectando URL diferentes

--- index.js
+++ index.js
@@ -1,10 +1,29 @@
 const http = require('http')
 
 let server = http.createServer((req, res)=>{
+    //linha de comando
     console.log('URL:', req.url)
     console.log('METHOD:', req.method)
 
-    res.end('OK')
+    switch(req.url){
+        case '/':
+            res.statusCode = 200 //código OK
+            res.setHeader('Content-Type', 'text/html') //cabeçalho
+            res.end('<h1>Olá</h1>')
+        break;
+
+        case '/users':
+            res.statusCode = 200 //código OK
+            res.setHeader('Content-Type', 'application/json') //cabeçalho
+            res.end(JSON.stringify({
+                users: [{
+                    name: 'Ana',
+                    email: 'aaaa@gmail.com',
+                    id: 1
+                }]
+            }))
+        break;
+    }
 })
 
 server.listen(3000, '127.0.0.1', ()=>{ //porta 3000, IP local
